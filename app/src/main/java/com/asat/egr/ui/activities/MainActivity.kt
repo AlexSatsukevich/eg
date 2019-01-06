1adde317189ab3db2d2586e1cfd07e010d4d0290
asats <a.satsukevich@live.com>
2019-01-06 14:00:55 +0300

    init project

---
+++ app/src/main/java/com/asat/egr/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.asat.egr.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.asat.egr.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
